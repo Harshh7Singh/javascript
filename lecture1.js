94ba6ef181d79e10c4725dfd98f69f0d727acf08
Harshh7Singh <142329215+Harshh7Singh@users.noreply.github.com>
2023-09-11 07:00:14 +0000

    container config done

--- lecture1.js
+++ lecture1.js
@@ -1,1 +1,20 @@
 console.log("Harsh")
+const accountId = 14454  //DON'T CHANGE
+let accountEmail = "Harsh@google.com"
+var accountpassword = "12345"
+accountcity = "LUCKNOW"
+let accountstate;
+/*
+prefer not to use var because of issue in block scope and unctional scope
+only used const and let only 
+but for changing value you should use let
+*/
+
+//accountId = 2
+
+console.log(accountId)
+accountEmail = "HARSH@.com"
+accountpassword = "5424"
+accountcity = "delhi"
+console.log(accountId)
+console.table([accountEmail,accountId,accountcity,accountpassword])
